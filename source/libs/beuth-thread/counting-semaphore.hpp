ec3a5415304e36ac2a3f55fe29e09893c6b122a8
tschwarzinger <s56930@beuth-hochschule.de>
2020-10-25 13:01:07 +0100

    Threading v2  - The current state of the threading library is fully documented (refs #3)

--- source/libs/beuth-thread/counting-semaphore.hpp
+++ source/libs/beuth-thread/counting-semaphore.hpp
@@ -9,20 +9,60 @@
 
 namespace beuth {
   namespace thread {
+    /**
+     * @brief Counting semaphore
+     *
+     * A semaphore, which is based on a counter.
+     *
+     * @author Tarek Schwarzinger
+     */
     struct CountingSemaphore {
       public:
+        /// Lock type definition
         using lock_type = std::unique_lock<std::mutex>;
 
+        /**
+         * @brief Constructor
+         *
+         * Constructs the counting semaphore and initializes the counter.
+         *
+         * @param init The initialization value for the counter. By default
+         *             the counter will be initialized with zero.
+         *
+         * @authors Christian Haß, Tarek Schwarzinger
+         */
         CountingSemaphore(std::ptrdiff_t init = 0)
           : count(init)
         {}
 
+        /**
+         * @brief Constructor
+         *
+         * Moves the counter of one semaphore to another one.
+         *
+         * @param src Source semaphore that contains the counter, that shall be moved.
+         *
+         * @authors Christian Haß, Tarek Schwarzinger
+         */
         CountingSemaphore(CountingSemaphore&& src) {
           count.store(src.count);
         }
 
         CountingSemaphore& operator=(CountingSemaphore&& src) = delete;
 
+        /**
+         * @brief take
+         *
+         * Takes values from the counter as soon as values are available. Otherwise
+         * this function waits for availability of the counter.
+         *
+         * @param n The amount that shall be taken from the counter.
+         *
+         * @attention This is a blocking call, that will wait until the full amount n could
+         *            be taken from the counter.
+         *
+         * @authors Christian Haß, Tarek Schwarzinger
+         */
         void take( std::size_t n=1 ) {
           if ( n==0 ) {
             return;
@@ -43,6 +83,18 @@ namespace beuth {
           });
         }
 
+        /**
+         * @brief wait
+         *
+         * Waits for a certain counter predicate to be fullfilled.
+         *
+         * @param predicate The predicate that needs to be fullfilled.
+         *
+         * @attention This is a blocking call, that will wait until the predicate
+         *            is fullfilled.
+         *
+         * @authors Christian Haß, Tarek Schwarzinger
+         */
         void wait(std::function<bool(std::ptrdiff_t)> predicate) {
           if (!predicate) {
             return;
@@ -54,6 +106,15 @@ namespace beuth {
           });
         }
 
+        /**
+         * @brief give
+         *
+         * Gives an amount to the counter and notifies all waiting threads.
+         *
+         * @param n The amount that shall be added to the counter.
+         *
+         * @authors Christian Haß, Tarek Schwarzinger
+         */
         void give( std::size_t n=1 ) {
           if ( n==0 ) {
             return;
@@ -66,26 +127,25 @@ namespace beuth {
           cv.notify_all();
         }
 
-        // reduce the count without waiting for it
-        void reduce(std::size_t n=1) {
-          if ( n==0 ) {
-            return;
-          }
-
-          {
-            lock_type l = lock_type(m);
-            count -= n;
-          }
-          cv.notify_all();
-        }
-
+        /**
+         * @brief operator std::ptrdiff_t
+         *
+         * @return Returns the current value of the counter.
+         *
+         * @author Tarek Schwarzinger
+         */
         inline operator std::ptrdiff_t() const {
           return count;
         }
 
       protected:
+        /// Mutex to guard the counter
         std::mutex m;
+
+        /// Condition variable to wait for
         std::condition_variable cv;
+
+        /// The counter
         std::atomic_ptrdiff_t count;
     };
   }
